3c09beaf75f5d95504726d29d125e09a8bfdc950
aoneban <torgregion1@gmail.com>
2024-11-27 13:30:51 +0100

    feat: add burger menu actions to the project

--- christmas-shop/src/js/gifts.js
+++ christmas-shop/src/js/gifts.js
@@ -4,3 +4,41 @@ import '../scss/styles.scss';
   const classValue = document.querySelector('.gifts');
   classValue.classList.add('addition');
 })();
+
+const burger = document.getElementById('burger');
+const content = document.querySelector('.overlay-content');
+burger.addEventListener(
+  'click',
+  (function () {
+    let flag = false;
+
+    return function () {
+      window.addEventListener('resize', () => {
+        if(window.innerWidth > 768) {
+          close();
+        }
+      })
+      content.addEventListener('click', (event) => {
+        if (event.target) {
+          close();
+        }
+      });
+
+      function open() {
+        burger.classList.add('change');
+        document.getElementById('myNav').style.width = '100%';
+        document.body.style.overflow = 'hidden';
+        flag = true;
+      }
+
+      function close() {
+        burger.classList.remove('change');
+        document.getElementById('myNav').style.width = '0';
+        document.body.style.overflow = 'visible';
+        flag = false;
+      }
+
+      return !flag ? open() : close();
+    };
+  })()
+);

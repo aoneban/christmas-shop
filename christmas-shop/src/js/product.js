ca1aa2a02d62540ed4fa8d72d848069a9d7aa55b
aoneban <torgregion1@gmail.com>
2024-11-30 20:20:50 +0100

    feat: continue doing on modal window

--- christmas-shop/src/js/product.js
+++ christmas-shop/src/js/product.js
@@ -7,10 +7,16 @@ export class ProductCard {
     this.image = image;
     this.category = category;
     this.description = description;
-    this.superpowers = superpowers
+    this.superpowers = superpowers;
   }
 
   createProductCard() {
+    const card = this.cartTemplate();
+    card.addEventListener('click', this.createModalCard.bind(this));
+    return card; 
+  }
+
+  cartTemplate() {
     const wrapper = document.createElement('div');
     wrapper.classList.add('gifts__products-wrapper');
 
@@ -30,40 +36,23 @@ export class ProductCard {
 
     gift.append(category, title);
     wrapper.append(img, gift);
-
-    wrapper.addEventListener('click', this.createModalCard.bind(this));
-
     return wrapper;
   }
 
   createModalCard() {
+
+    const injectCart = this.cartTemplate();
+
     const wrapper = document.createElement('div');
-    wrapper.classList.add('gifts__modal-wrapper');
-
-    const img = document.createElement('img');
-    img.src = this.image;
-    img.setAttribute('alt', this.name);
-
-    const gift = document.createElement('div');
-    gift.classList.add('gifts__products-name');
-
-    const category = document.createElement('p');
-    category.textContent = this.category;
-    category.style.color = this.color;
+    wrapper.classList.add('wrapper');
 
     const description = document.createElement('p');
     description.textContent = this.description;
 
-    const title = document.createElement('h4');
-    title.textContent = this.name;
-
-    gift.append(category, title, description);
-    wrapper.append(img, gift);
-
     const modal = document.createElement('div');
     modal.setAttribute('id', 'myModal');
     modal.setAttribute('class', 'modal');
-    modal.style.display = "block";
+    modal.style.display = 'block';
 
     const close = document.createElement('span');
     close.classList.add('close');
@@ -72,8 +61,10 @@ export class ProductCard {
     const content = document.createElement('div');
     content.classList.add('modal-content');
 
-    content.append(wrapper);
-    modal.append(content, close);
+    wrapper.append(injectCart, close)
+
+    content.append(wrapper, description);
+    modal.append(content);
     document.body.appendChild(modal);
 
     const closeModal = () => {
@@ -84,6 +75,3 @@ export class ProductCard {
     return modal;
   }
 }
-
-
-
